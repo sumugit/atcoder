4afe13263941da757107df5e7e513edc7074960c
sumugit <diddy2983@yahoo.co.jp>
2024-01-22 23:24:58 +0900

    chapter8

---
+++ tessoku/chapter8/A53.py
@@ -0,0 +1,21 @@
+from collections import Counter, deque, defaultdict
+from heapq import heappush, heappop, heappushpop, heapify, heapreplace, merge
+from bisect import bisect_left, bisect_right, bisect, insort_left, insort_right, insort
+from itertools import accumulate, product, permutations, combinations
+
+Q = int(input())
+query = [list(input().split()) for _ in range(Q)]
+
+queue = []
+
+for q in query:
+    if len(q) == 1:
+        num = q[0]
+    else:
+        num, x = q
+    if num == "1":
+        heappush(queue, int(x))
+    elif num == "2":
+        print(queue[0])
+    else:
+        heappop(queue)
